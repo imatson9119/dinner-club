1369c366058ae34e5b782b24813de848ea7d693b
Ian Matson <imatson9119@gmail.com>
2023-10-05 17:48:48 -0500

    Style updates'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'dinner-club';
   timeRemaining = new Date().getTime() 
   dropDate = new Date(2023,9,21,0,0,0).getTime()
+  // dropDate = new Date(2023,9,5,17,55,0).getTime()
   
     constructor() {
         setInterval(() => {
@@ -20,17 +21,29 @@ export class AppComponent {
     getDateString(distance){
       let build = ""
       let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      build += days
-      build = days == 1 ? build + " days " : build + " day "
       let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      build += hours
-      build = hours == 1 ? build + " hours " : build + " hour "
       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      build += minutes
-      build = minutes == 1 ? build + " minutes " : build + " minute "
       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      build += seconds
-      build = seconds == 1 ? build + " seconds" : build + " second"
+      if(days === 1){
+        build += days + " day, "
+      } else if(days >= 1) {
+        build += days + " days, "
+      }
+      if(hours === 1){
+        build += hours + " hour, "
+      } else if(hours >= 1  || !(days >= 0)) {
+        build += hours + " hours, "
+      }
+      if(minutes === 1){
+        build += minutes + " minute and "
+      } else if(minutes >= 1 || !(hours >= 0 && days >= 0)) {
+        build += minutes + " minutes and "
+      }
+      if(seconds === 1){
+        build += seconds + " second "
+      } else {
+        build += seconds + " seconds "
+      }
       return build
     }
 }
